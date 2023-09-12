39dde810127c87007c941790125b950c75938570
COYO <dayeon.yoon@gmail.com>
2023-09-13 00:44:34 +0900

    Deploy Setting

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/rendering_engine_practice/",
   plugins: [react(), svgr()],
   resolve: {
     alias: [
